924c4358b5179220f926253fda13c4d7a8ea6c00
Tomori_Nao <zjhzjhanpanman@gmail.com>
2023-10-19 20:11:01 +0800

    99% finished?

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -20,9 +20,7 @@
 // your own code, take a step back and use safe code instead!
 //
 // Execute `rustlings hint tests5` or use the `hint` watch subcommand for a
-// hint.
 
-// I AM NOT DONE
 
 /// # Safety
 ///
@@ -32,7 +30,10 @@ unsafe fn modify_by_address(address: usize) {
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
     unsafe {
-        todo!("Your code goes here")
+        // todo!("Your code goes here")
+        // address = *address as usize;
+        let ptr = address as *mut u32;
+        *ptr = 0xAABBCCDD;
     }
 }
 
